b986408fedcade8e048f8f08c4be18ea23a858be
Scott Shaffer <jsshaff@berkeley.edu>
2018-02-04 12:25:26 -0800

    New locations and environment variables

--- bin/amat-driver.sh
+++ bin/amat-driver.sh
@@ -3,9 +3,15 @@
 # Usage: amat-driver.sh FILE1 [FILE2 ...]
 ########################################################################
 
+if [ -z "${JAM_HOME}" ]
+then
+    echo "Environment variable JAM_HOME is not set; exiting."
+    exit 1
+fi
+
 SCRIPT=`basename $0`
 JAMDIR=$(cd `dirname $0`/../../jam; pwd)
-JAMRUN=${JAMDIR}/bin/jam-run.sh
+JAMRUN=${JAM_HOME}/bin/jam-run.sh
 
 if [ $# -lt 1 ]
 then
@@ -13,4 +19,4 @@ then
     exit 1
 fi
 
-$JAMRUN amat amat.driver.AmatDriver "$@"
+$JAMRUN ${AMAT_HOME} amat.driver.AmatDriver "$@"
